da8786a28c93bd016b2efe1291a8322ffc1eb240
mingit55 <mingit55@gmail.com>
2020-08-13 21:21:27 +0900

    찾아오는 길 완료

--- js/common.js
+++ js/common.js
@@ -9,5 +9,20 @@ location.getQueryString = function() {
 };
 
 $(function(){
-    
+    $("[data-target='#road-modal']").on("click", e => {
+        e.preventDefault();
+        let timeout = false;
+        fetch("/location.php")
+            .then(res => res.text())
+            .then(resText => {
+                if(!timeout) {
+                    $("#road-modal .modal-body").html( $(resText) );
+                    $("#road-modal").modal("show");
+                }
+            });
+        setTimeout(() => {
+            timeout = true;
+            alert("돌아오는 길을 표시할 수 없습니다.");
+        }, 1000);
+    });
 });
